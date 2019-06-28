4f030112806026150bc249f93fe9b04c89255acd
Rohan <rohan.sharma@ziploan.in>
2019-06-28 17:50:17 +0530

    remove not needed exceptions

--- vayana_modules/exceptions.py
+++ vayana_modules/exceptions.py
@@ -4,11 +4,3 @@ class VayanaAuthException(Exception):
 
 class APIException(Exception):
     pass
-
-
-class DecoderException(Exception):
-    pass
-
-
-class TransformerException(Exception):
-    pass
