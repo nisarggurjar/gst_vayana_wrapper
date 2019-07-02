654a48cfded2deb3c3a93f5d60623f14c3580537
Rohan <rohan.sharma@ziploan.in>
2019-07-02 12:43:56 +0530

    inherit from base exception and modify APIExceptions ability to store error_code and message separately

--- vayana_modules/exceptions.py
+++ vayana_modules/exceptions.py
@@ -3,4 +3,13 @@ class VayanaAuthException(Exception):
 
 
 class APIException(Exception):
-    pass
+
+    def __init__(self, json_data):
+        self.message = json_data['message']
+        self.error_code = json_data['error_cd']
+
+    def __str__(self):
+        return self.message + ":" + self.error_code
+
+    def __repr__(self):
+        return self.message + ":" + self.error_code
