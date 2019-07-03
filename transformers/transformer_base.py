93759001a32cd91fdb1a686306258a5e45b7c7df
Rohan <rohan.sharma@ziploan.in>
2019-07-03 12:00:35 +0530

    add support for delete keys transformation

--- transformers/transformer_base.py
+++ transformers/transformer_base.py
@@ -32,17 +32,64 @@ class TransformerBase(object):
 
         current_key = sections[0]
         remaining_path = ".".join(sections[1:])
-        containing_frame = containing_frame[current_key]
+
+        # Ignore wrong paths
+        try:
+            containing_frame = containing_frame[current_key]
+        except KeyError:
+            return
+
         self._rename_keys(containing_frame, remaining_path, new_name)
 
     def _rename_key(self, frame, key_name, new_name):
         frame[new_name] = frame[key_name]
         del frame[key_name]
 
-    def rename_transformation(self):
+    def _del_keys(self, containing_frame, path):
+
+        if isinstance(containing_frame, list):
+            for item in containing_frame:
+                self._del_keys(item, path)
+
+            return
+
+        sections = path.split(".")
+
+        if len(sections) == 1:
+            try:
+                del containing_frame[path]
+            except KeyError:
+                pass
+
+            return
+
+        current_key = sections[0]
+        remaining_path = ".".join(sections[1:])
+
+        # Ignore wrong paths
+        try:
+            containing_frame = containing_frame[current_key]
+        except KeyError:
+            return
+
+        self._del_keys(containing_frame, remaining_path)
+
+    def _rename_transformation(self):
 
         for path, new_name in self.RENAME_DATA.items():
             self._rename_keys(self.data, path, new_name)
+
+        return self.data
+
+    def _del_transformation(self):
+
+        try:
+            paths = getattr(self, "DEL_DATA")
+        except AttributeError:
+            return
+
+        for path in paths:
+            self._del_keys(self.data, path)
 
         return self.data
 
@@ -59,7 +106,8 @@ class TransformerBase(object):
         if self.transformed:
             return self.data
 
-        self.rename_transformation()
+        self._del_transformation()
+        self._rename_transformation()
         self.rearrange_transformation()
         self.transformed = True
 
