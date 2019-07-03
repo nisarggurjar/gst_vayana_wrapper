12e03d7bf24c612642b6e5c78b2b8adb5b3ad0ea
Rohan <rohan.sharma@ziploan.in>
2019-07-03 16:43:18 +0530

    fix json paths

--- transformers/list_returns_transformer.py
+++ transformers/list_returns_transformer.py
@@ -5,10 +5,10 @@ from transformers.transformer_base import TransformerBase
 class ListReturnsTransformer(TransformerBase):
 
     RENAME_DATA = OrderedDict([
-        ("ret_prd", "return_period"),
-        ("mof", "method_of_filing"),
-        ("dof", "date_of_filing"),
-        ("rtntype", "return_type"),
+        ("EFiledlist.ret_prd", "return_period"),
+        ("EFiledlist.mof", "method_of_filing"),
+        ("EFiledlist.dof", "date_of_filing"),
+        ("EFiledlist.rtntype", "return_type"),
     ])
 
     def rearrange_transformation(self):
